73ca2a10d75d4fb220fb88c6712911ba05159733
David Gageot <david@gageot.net>
2015-04-06 10:50:51 +0200

    Add logs for BigQuery demo

--- src/main/java/main/VoteResource.java
+++ src/main/java/main/VoteResource.java
@@ -2,10 +2,14 @@ package main;
 
 import net.codestory.http.annotations.Post;
 import net.codestory.http.annotations.Prefix;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import store.Votes;
 
 @Prefix("/votes")
 public class VoteResource {
+    private final Logger log = LoggerFactory.getLogger(VoteResource.class);
+
     private final Votes votes;
 
     public VoteResource(Votes votes) {
@@ -14,6 +18,8 @@ public class VoteResource {
 
     @Post(":winner/:looser")
     public void vote(int winner, int looser) {
+        log.info("MATCH;{};{};{};{};{};{}", winner, looser, votes.score(winner), votes.votes(winner), votes.score(looser), votes.votes(looser));
+
         votes.vote(winner, looser);
     }
 }
